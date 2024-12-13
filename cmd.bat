928485fe9064c01f045cfdaa41c5fb63c29a4064
Viktor123861234 <140442256+Viktor123861234@users.noreply.github.com>
2024-12-13 10:23:10 +0100

    Add files via upload

---
+++ cmd.bat
@@ -0,0 +1,19 @@
+@echo off
+echo --------------------POLECENIE PING--------------------
+ping 127.0.0.1
+echo --------------------POLECENIE IPCONFIG--------------------
+ipconfig
+echo --------------------POLECENIE NETSTAT--------------------
+netstat
+echo --------------------POLECENIE ARP--------------------
+arp -a
+echo --------------------POLECENIE NSLOOKUP--------------------
+nslookup www.google.com
+echo --------------------POLECENIE GETMAC------------------------
+getmac
+echo --------------------POLECENIE PATHPING--------------------
+pathping 127.0.0.1
+echo --------------------POLECENIE TRACERT--------------------
+tracert
+echo Na tym sie skoncze
+pause
